aae4067f2a1616936c89bd1f62e938db178cb3ef
j-saggu <jsaggu@ualberta.ca>
2023-09-13 18:24:04 -0600

    second commit

--- lab1.py
+++ lab1.py
@@ -2,4 +2,8 @@ import requests
 print(requests.__version__)
 #// https://pypi.org/project/requests/
 r = requests.get("http://google.com/")
-r.status_code
+print(r.status_code)
+# https://stackoverflow.com/questions/14120502/how-to-download-and-write-a-file-from-github-using-requests 
+r = requests.get("https://raw.githubusercontent.com/j-Saggu/Lab1/main/lab1.py")
+print(r.status_code)
+
